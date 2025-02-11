6e98959a0fa783acfdceb911e71d0e368cef712b
rfael420 <155620317+rfael420@users.noreply.github.com>
2025-02-11 21:09:39 +0800

    Update TSA2_BUSCANO.js

--- Buscano_TSA2/TSA2_BUSCANO.js
+++ Buscano_TSA2/TSA2_BUSCANO.js
@@ -1,1 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    function showBox(dayClass) {
+        document.querySelectorAll(".box-container div").forEach(box => {
+            box.classList.remove("active");
+        });
+
+        const selectedBox = document.querySelector(`.box-${dayClass}`);
+        if (selectedBox) {
+            selectedBox.classList.add("active");
+        }
+    }
+
+    document.querySelectorAll(".button").forEach(button => {
+        button.addEventListener("click", function () {
+            const dayClass = this.classList[1].split("-")[1]; 
+            showBox(dayClass);
+        });
+    });
+});
 
